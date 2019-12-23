9ca074e3ed874b942a3d7e8651489c97e18077f2
DrizzleAssistant <58386260+DrizzleAssistant@users.noreply.github.com>
2019-12-23 15:27:25 +0200

    Create cmds.py

---
+++ cmds/cmds.py
@@ -0,0 +1,33 @@
+import asyncio
+import discord
+from discord.ext import commands
+
+from core import checks
+from core.models import PermissionLevel
+
+
+class cmds(commands.Cog):
+    """
+    A sample set of rules you can use when your first starting your server to save time.
+    """
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command(aliases=["help"])
+    @checks.has_permissions(PermissionLevel.REGULAR)
+    async def cmds(self, ctx):
+        """"""
+        embed = discord.Embed(
+            title=" "
+        )
+        embed.set_author(name=str(ctx.author), icon_url=str(ctx.author.avatar_url))
+        embed.description = """
+                You can view my commands [here.](https://github.com/DrizzleAssistant/DrizzleBots/blob/master/announcement-cmds.md)
+            """
+        embed.set_footer(text="These commands can only be used by a SHR.")
+        embed.color = self.bot.main_color
+        return await ctx.send(embed=embed)
+
+def setup(bot):
+    bot.add_cog(cmds(bot))
